41a5fcf448a87ebaf27038ac398269a648a548d8
Michael Onubogu <nonubogu@gmail.com>
2016-01-05 14:21:52 -0500

    google places api test

--- test.js
+++ test.js
@@ -1,3 +1,23 @@
 (function(){
-    var google_places_api_key ='AIzaSyAbCvTAUeIsqyjYnshNbqUBz93r-ExvXE4';
+    $(document).ready(function(){
+        var map;
+        var infowindow;
+        
+        
+        var pyrmont = {lat: -33.867, lng: 151.195};
+
+        map = new google.maps.Map(document.getElementById('map'), {
+            center: pyrmont,
+            zoom: 15
+        });
+        
+        var service = new google.maps.places.PlacesService(map);
+        service.nearbySearch({
+            location: pyrmont,
+            radius: 500,
+            types: ['store']
+        }, function(results, status){
+            console.log(results);
+        });
+    });
 })();
